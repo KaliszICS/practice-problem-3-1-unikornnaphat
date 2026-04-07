0f6a42ddfda316c6791d9b8a86940e386768dab9
unikornnaphat <350364092@tdsb.ca>
2026-04-07 01:39:02 +0000

    3.1-sandy

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,31 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+    }
 
-	}
+    public static String evenOrOdd(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
 
+	public static String teacherOrStudent(String str) {
+        if (str.equals("Kalisz")) {
+            return "Teacher";
+        } else {
+            return "Student";
+        }
+    }
+
+	public static int fartherFromZero(int num) {
+        if (num > 0) {
+            return num + 5;
+        } else if (num < 0) {
+            return num - 5;
+        } else {
+            return 0;
+        }
+    }
 }
